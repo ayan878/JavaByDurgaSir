173daa2897806c5c39274bbd42b6ccd016525b7b
ayan878 <ayanraza197@gmail.com>
2023-02-12 21:37:17 +0530

    First Java DurgaSir Program

---
+++ OPPS/TightlyCapsualted.java
@@ -0,0 +1,22 @@
+
+
+class Account 
+{
+    
+private double balance;
+public double getBalance() {
+return balance;
+}
+}
+
+class A {
+int x=10; //not
+}
+class B extends A {
+private int y=20; //not
+}
+class C extends B {
+private int z=30; //not
+}
+//Note: if the parent class is not tightly encapsulated then no child class is tightly
+//encapsulated.
